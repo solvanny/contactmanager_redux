0f5deddb45901213265560472cd59005f7b5de0d
Ion Soltan <ion.soltan@hotmail.com>
2019-01-06 11:00:09 -0500

    POST_DELETE request dispatch

--- src/actions/contactActions.js
+++ src/actions/contactActions.js
@@ -1,21 +1,27 @@
 import { GET_CONTACTS, DELETE_CONTACT, ADD_CONTACT } from './types';
+import axios from 'axios';
 
-export const getContacts = () => {
-  return {
-    type: GET_CONTACTS
-  }
+export const getContacts = () => async  dispatch => {
+  let res = await axios.get('https://jsonplaceholder.typicode.com/users')
+  dispatch ({
+    type: GET_CONTACTS,
+    payload: res.data
+  });
 }
 
-export const deleteContact = (id) => {
-  return {
+export const deleteContact = id => async dispatch => {
+  await axios.delete(`https://jsonplaceholder.typicode.com/users/${id}`)
+  dispatch({
     type: DELETE_CONTACT,
     payload: id
-  }
+  });
 }
 
-export const addContact = (contact) => {
-  return {
+export const addContact = contact => async dispatch => {
+
+  let res = await axios.post('https://jsonplaceholder.typicode.com/users', contact)
+  dispatch({
     type: ADD_CONTACT,
-    payload: contact
-  }
+    payload: res.data
+  });
 }
